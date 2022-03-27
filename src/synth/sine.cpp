d743a26e67d9d8325955a5d6db5bc997d8466afb
lucmans <lsdejonckheere@gmail.com>
2022-03-27 12:50:52 +0200

    use offset+length info for sine synthesis

--- src/synth/sine.cpp
+++ src/synth/sine.cpp
@@ -4,6 +4,8 @@
 #include "error.h"
 #include "config/audio.h"
 
+#include <algorithm>  // std::fill_n()
+
 
 Sine::Sine() {
     last_phase = 0.0;
@@ -14,29 +16,40 @@ Sine::~Sine() {
 }
 
 
-// TODO: Support NoteEvent timing data instead of playing the note the whole buffer
 void Sine::synthesize(const NoteEvents &note_events, float *const synth_buffer, const int n_samples) {
     const int n_events = note_events.size();
 
     // Output silence if there are no notes
     if(n_events == 0) {
-        for(int i = 0; i < n_samples; i++)
-            synth_buffer[i] = 0.0;
-
+        std::fill_n(synth_buffer, n_samples, 0.0);  // memset() might be faster, but assumes IEEE 754 floats/doubles
         return;
     }
 
-    int note_idx = 0;
+    // Find relevant event from note_events
+    int event_idx = 0;
     if(n_events > 1) {
         warning("Polyphony not yet supported; playing loudest note instead");  // TODO: Support
         for(int i = 1; i < n_events; i++)
-            if(note_events[i].note.amp > note_events[note_idx].note.amp)
-                note_idx = i;
+            if(note_events[i].note.amp > note_events[event_idx].note.amp)
+                event_idx = i;
+    }
+    const NoteEvent &out_event = note_events[event_idx];
+
+    // DEBUG: Sanity check
+    if(out_event.offset + out_event.length > (unsigned int)n_samples) {
+        error("Note event passed to synthesizer is longer than the synth_buffer\nOr the synth_buffer was created shorter than input_buffer_n_samples or Estimator gave note event information from beyond its buffer.");
+        exit(EXIT_FAILURE);
     }
 
-    const double offset = (last_phase * ((double)SAMPLE_RATE / note_events[note_idx].note.freq));
-    for(int i = 0; i < n_samples; i++)
-        synth_buffer[i] = sinf((2.0 * M_PI * ((double)i + offset) * note_events[note_idx].note.freq) / (double)SAMPLE_RATE);
+    // Write samples to buffer
+    const Note &out_note = out_event.note;
+    const double phase_offset = (last_phase * ((double)SAMPLE_RATE / out_note.freq));
+    for(unsigned int i = out_event.offset; i < out_event.offset + out_event.length; i++)
+        synth_buffer[i] = sinf((2.0 * M_PI * ((double)i + phase_offset) * out_note.freq) / (double)SAMPLE_RATE);
 
-    last_phase = fmod(last_phase + (note_events[note_idx].note.freq / ((double)SAMPLE_RATE / (double)n_samples)), 1.0);
+    last_phase = fmod(last_phase + (out_note.freq / ((double)SAMPLE_RATE / (double)n_samples)), 1.0);
+
+    // Fill silent part with zeros
+    std::fill_n(synth_buffer, out_event.offset, 0.0);  // Start
+    std::fill_n(synth_buffer + out_event.offset + out_event.length, n_samples - (out_event.offset + out_event.length), 0.0);  // End
 }
