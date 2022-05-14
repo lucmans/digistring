6c69550e9261078fca9532024709f653e9f97040
lucmans <lsdejonckheere@gmail.com>
2022-05-15 00:34:06 +0200

    Removed power scaling from amped synth

--- src/config/synth.h
+++ src/config/synth.h
@@ -3,7 +3,7 @@
 
 
 // Let AMP_SCALING be x, the amplitude of the out wave is calculated using note.amp^x / max_amp^x
-constexpr double AMP_SCALING = 3.5;
+constexpr double AMP_SCALING = 1.0;
 
 
 #endif  // DIGISTRING_CONFIG_SYNTH_H
